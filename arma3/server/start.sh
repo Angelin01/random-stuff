8df371ca54397bc359c843a21d2998ab2636a174
Angelo Ross <angelo.ross@objective.com.br>
2025-03-28 14:41:46 -0300

    Add simple backup script

--- arma3/server/start.sh
+++ arma3/server/start.sh
@@ -8,7 +8,12 @@ cd "$(dirname "$(readlink -f "$0")")"
 
 MODS_FOLDER=${MODS_FOLDER:?"Mods folder env var MODS_FOLDER is undefined"}
 SERVER_MODS_FOLDER=${SERVER_MODS_FOLDER:?"Server mods folder env var SERVER_MODS_FOLDER is undefined"}
+SAVES_BACKUP_DIR=/opt/games-backup/arma3
 
+echo "Backing up saves"
+rsync -av "$SERVER_MODS_FOLDER/filext/storage/" "$SAVES_BACKUP_DIR"
+
+echo "Running server"
 exec ./arma3server_x64 \
   -config=server.cfg \
   -servermod="$(printf "%s;" "$SERVER_MODS_FOLDER"/*)" \
