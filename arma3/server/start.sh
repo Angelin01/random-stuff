73ff735108418ee9de60726767b0079a0d34de9e
Angelo Ross <angelo.ross@objective.com.br>
2025-03-28 14:25:45 -0300

    Add update mods script

--- arma3/server/start.sh
+++ arma3/server/start.sh
@@ -9,9 +9,6 @@ cd "$(dirname "$(readlink -f "$0")")"
 MODS_FOLDER=${MODS_FOLDER:?"Mods folder env var MODS_FOLDER is undefined"}
 SERVER_MODS_FOLDER=${SERVER_MODS_FOLDER:?"Server mods folder env var SERVER_MODS_FOLDER is undefined"}
 
-find "$MODS_FOLDER" -depth -exec rename -v 's|(.*)/(.*)|$1/\L$2|' {} +
-find "$SERVER_MODS_FOLDER" -depth -exec rename -v 's|(.*)/(.*)|$1/\L$2|' {} +
-
 exec ./arma3server_x64 \
   -config=server.cfg \
   -servermod="$(printf "%s;" "$SERVER_MODS_FOLDER"/*)" \
